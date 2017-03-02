d19fe679ea6e6e3dbb6142e9d2a36e07e5104641
张瑞兰 <zhangruilan007@gmail.com>
2017-03-02 15:36:16 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new :create]
+  before_action :authenticate_user! , only: [:new, :create]
   def index
     @group = Group.all
   end
