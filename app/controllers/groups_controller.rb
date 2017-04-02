88fb1939818e7bbb882e62cc9fb8e28780188b91
张瑞兰 <zhangruilan007@gmail.com>
2017-04-02 14:56:50 +0800

    revise groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,19 +21,15 @@ end
 
   def create
     @group = Group.new(group_params)
-  #  @group.user = current_user
+   @group.user = current_user
 
-    #if @group.save
-      #current_user.join!(@group)
-      #redirect_to groups_path
-    #else
-      #render :new
-    #end
+    if @group.save
+      current_user.join!(@group)
+      redirect_to groups_path
+    else
+      render :new
+    end
 
-    @group.save
-
-    redirect_to groups_path
-  end
 
   def update
 
